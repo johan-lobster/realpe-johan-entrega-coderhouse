ec83f4b4ff809400fb18facc5c9f3e3f194f2934
Johan Realpe <relpesebastian1502@gmail.com>
2025-04-14 22:45:26 -0500

    se crea la vista para "crear estudinte y se le asigna un boton para la busqueda"

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from home.views import inicio
+from home.views import crear_estudiante
 
 urlpatterns = [
-    path('', inicio, name = 'inicio')
+    path('', inicio, name = 'inicio'),
+    path('estudiante/crear', crear_estudiante, name = 'crear_estudiante')
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'home/inicio.html')
+
+def crear_estudiante(request):
+    return render(request, 'home/crear_estudiante.html')
