7456eb61efb542f8a149f7f7d97d393f06fc8a5c
Johan Realpe <relpesebastian1502@gmail.com>
2025-05-01 20:27:03 -0500

    se mejora el registro del formulario para poder agregar más campos y una ayida visual que permite saber si la sesion está iniciada

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as django_login
+from usuarios.forms import FormularioRegistro
 
 def login(request):
 
@@ -18,12 +19,12 @@ def login(request):
 
 def registro(request):
     if request.method == "POST":
-        formulario = UserCreationForm(request.POST)
+        formulario = FormularioRegistro(request.POST)
         if formulario.is_valid():
             formulario.save()
 
             return redirect("login")
     else:
-        formulario = UserCreationForm()
+        formulario = FormularioRegistro()
 
     return render(request, 'usuarios/registro.html', {'formulario': formulario})
