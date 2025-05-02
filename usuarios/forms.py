bd123c4c649bbb4b6ed6e4f3c7bfb6be759c286e
Johan Realpe <relpesebastian1502@gmail.com>
2025-05-02 18:57:59 -0500

    desarrollamos la seccion de "editar perfl

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from django.contrib.auth.models import User
 
@@ -12,3 +12,14 @@ class FormularioRegistro(UserCreationForm):
         model = User
         fields = ["username", "email", "password1", "password2"]
         helps_text = {llave: "" for llave in fields}
+
+
+class FromularioEdicionPerfil(UserChangeForm):
+    password = None
+    email = forms.EmailField(required=False)
+    first_name = forms.CharField(label="Nombre")
+    last_name = forms.CharField(label="Apellido")
+
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name"]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django.contrib.auth import login as django_login
-from usuarios.forms import FormularioRegistro
+from usuarios.forms import FormularioRegistro, FromularioEdicionPerfil
+from django.contrib.auth.decorators import login_required
 
 def login(request):
 
@@ -28,3 +29,17 @@ def registro(request):
         formulario = FormularioRegistro()
 
     return render(request, 'usuarios/registro.html', {'formulario': formulario})
+
+
+@login_required
+def editar_perfil(request):
+    if request.method == "POST":
+        formulario = FromularioEdicionPerfil(request.POST, instance=request.user)
+        if formulario.is_valid():
+            formulario.save()
+
+            return redirect("inicio")
+    else:
+        formulario = FromularioEdicionPerfil(instance=request.user)
+
+    return render(request, 'usuarios/editar_perfil.html', {'formulario': formulario})
