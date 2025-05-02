4d8a2ad316a3f7ccba1da643766927733953832f
Johan Realpe <relpesebastian1502@gmail.com>
2025-05-01 19:34:38 -0500

    se agrega la función de registrarse con un formulario que al ser valido te redirige a login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as django_login
 
 def login(request):
@@ -15,3 +15,15 @@ def login(request):
         formulario = AuthenticationForm()
 
     return render(request, 'usuarios/login.html', {'formulario': formulario})
+
+def registro(request):
+    if request.method == "POST":
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+
+            return redirect("login")
+    else:
+        formulario = UserCreationForm()
+
+    return render(request, 'usuarios/registro.html', {'formulario': formulario})
